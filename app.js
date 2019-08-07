a0b3264893aaad5b838a69e7058de3bf664a7d75
Justin <whackybeanz@gmail.com>
2019-08-07 15:33:46 +0800

    Added initial folders and files, npm packages

---
+++ app.js
@@ -0,0 +1,20 @@
+var express 				= require("express");
+var app						= express();
+
+var indexRoutes 	= require("./routes/index");
+
+var port = process.env.PORT || 3005;
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+// ROUTES
+app.use("/", indexRoutes);
+
+app.get("*", function(req, res) {
+	req.flash("error", "Invalid route accessed.")
+	res.redirect("back");
+})
+
+app.listen(port, function() {
+	console.log("Server is running");
+})
