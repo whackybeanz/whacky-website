561b4743ae4e8716d91b85d9ca5af82e34cb7552
Justin <whackybeanz@gmail.com>
2019-08-13 17:29:34 +0800

    Updated scripts to adjust displayed values based on input options

--- public/scripts/flameScript.js
+++ public/scripts/flameScript.js
@@ -3,14 +3,21 @@ $(".equip-type-input").on("click", function() {
 	$(this).addClass("active");
 })
 
+$(".flame-type-input").on("click", function() {
+	$(".flame-type-input").removeClass("active");
+	$(this).addClass("active");
+})
+
+$(".stat-type-input").on("click", function() {
+	$(".stat-type-input").removeClass("active");
+	$(this).addClass("active");
+})
+
 $("#flame-submit-btn").on("click", function(event) {
 	event.preventDefault();
 
-	var normalWAMA = [1, 2.2, 3.65, 5.35, 7.3, 8.8, 10.25];
-	var bossWAMA = [0, 0, 3, 4.4, 6.05, 8, 10.25];
-
 	var itemLevel = Number($("#gear-level").val());
-	var weaponWAMA = Number($("#base-wa-ma").val());
+	var inputWAMA = Number($("#base-wa-ma").val());
 
 	var pureStatsFactor = Math.floor(itemLevel/20) + 1; // Pure stats, def stats
 	var mixedStatsFactor = Math.floor(itemLevel/40) + 1; // Mixed stats
@@ -22,37 +29,81 @@ $("#flame-submit-btn").on("click", function(event) {
 		hpMpFactor = Math.floor(itemLevel/10);
 	}
 
+	displayOutput(inputWAMA, pureStatsFactor, mixedStatsFactor, hpMpFactor);
+})
+
+function displayOutput(inputWAMA, pureStatsFactor, mixedStatsFactor, hpMpFactor) {
+	var equipType = $(".equip-type-input.active").data("equipType");
+	var flameType = $(".flame-type-input.active").data("flameType");
+	var statType = $(".stat-type-input.active").data("statType");
+
+	var normalWAMA = [1, 2.2, 3.65, 5.35, 7.3, 8.8, 10.25];
+	var bossWAMA = [0, 0, 3, 4.4, 6.05, 8, 10.25];
+
+	var typeOfWAMA;
+	var minFlameTier = 0;
+	var maxFlameTier = 0;
+
+	if(equipType === "normal") {
+		if(flameType === "crimson") {
+			minFlameTier = 1;
+			maxFlameTier = 4;
+		} else if(flameType === "rainbow") {
+			minFlameTier = 2;
+			maxFlameTier = 5;
+		}
+
+		typeOfWAMA = normalWAMA;
+	} else if(equipType === "boss") {
+		if(flameType === "crimson") {
+			minFlameTier = 3;
+			maxFlameTier = 6;
+		} else if(flameType === "rainbow") {
+			minFlameTier = 4;
+			maxFlameTier = 7;
+		}
+
+		typeOfWAMA = bossWAMA;
+	}
+
 	for(var i = 1; i <= 7; i++) {
-		// Pure Stats
-		$(`.pure-stats-output.flame-tier-${i}`).text(pureStatsFactor * i);
-
-		// Mixed Stats
-		$(`.mixed-stats-output.flame-tier-${i}`).text(mixedStatsFactor * i);
-
-		// HP/MP
-		$(`.hp-mp-output.flame-tier-${i}`).text(hpMpFactor * i * 30);
-
-		// Others
-		if(weaponWAMA === 0) {
+		if(i < minFlameTier || i > maxFlameTier) {
 			$(`.weapon-wa-ma-output.flame-tier-${i}`).text("-");
 			$(`.boss-dmg-output.flame-tier-${i}`).text("-");
+			$(`.pure-stats-output.flame-tier-${i}`).text("-");
+			$(`.mixed-stats-output.flame-tier-${i}`).text("-");
+			$(`.hp-mp-output.flame-tier-${i}`).text("-");
+			$(`.all-stats-output.flame-tier-${i}`).text("-");
+			$(`.damage-output.flame-tier-${i}`).text("-");
+			$(`.armor-wa-ma-output.flame-tier-${i}`).text("-");
+			$(`.speed-jump-output.flame-tier-${i}`).text("-");
+			$(`.defense-output.flame-tier-${i}`).text("-")
+			$(`.level-reduce-output.flame-tier-${i}`).text("-");
 		} else {
-			var calculatedWAMA = Math.ceil(weaponWAMA * (bossWAMA[i-1] * mixedStatsFactor / 100));
-
-			if(calculatedWAMA === 0) {
+			if(inputWAMA === 0) {
 				$(`.weapon-wa-ma-output.flame-tier-${i}`).text("-");
+				$(`.boss-dmg-output.flame-tier-${i}`).text("-");
 			} else {
-				$(`.weapon-wa-ma-output.flame-tier-${i}`).text(calculatedWAMA);
+				var calculatedWAMA = Math.ceil(inputWAMA * (typeOfWAMA[i-1] * mixedStatsFactor / 100));
+
+				if(calculatedWAMA === 0) {
+					$(`.weapon-wa-ma-output.flame-tier-${i}`).text("-");
+				} else {
+					$(`.weapon-wa-ma-output.flame-tier-${i}`).text(calculatedWAMA);
+				}
+				
+				$(`.boss-dmg-output.flame-tier-${i}`).text(`${2 * i}%`);
 			}
 			
-			$(`.boss-dmg-output.flame-tier-${i}`).text(`${2 * i}%`);
+			$(`.pure-stats-output.flame-tier-${i}`).text(pureStatsFactor * i);
+			$(`.mixed-stats-output.flame-tier-${i}`).text(mixedStatsFactor * i);
+			$(`.hp-mp-output.flame-tier-${i}`).text(hpMpFactor * i * 30);
+			$(`.all-stats-output.flame-tier-${i}`).text(`${i}%`);
+			$(`.damage-output.flame-tier-${i}`).text(`${i}%`);
+			$(`.armor-wa-ma-output.flame-tier-${i}`).text(i);
+			$(`.speed-jump-output.flame-tier-${i}`).text(i);
+			$(`.defense-output.flame-tier-${i}`).text(pureStatsFactor * i)
+			$(`.level-reduce-output.flame-tier-${i}`).text(-5 * i);
 		}
-		
-		$(`.all-stats-output.flame-tier-${i}`).text(`${i}%`);
-		$(`.damage-output.flame-tier-${i}`).text(`${i}%`);
-		$(`.armor-wa-ma-output.flame-tier-${i}`).text(i);
-		$(`.speed-jump-output.flame-tier-${i}`).text(i);
-		$(`.defense-output.flame-tier-${i}`).text(pureStatsFactor * i)
-		$(`.level-reduce-output.flame-tier-${i}`).text(-5 * i);
 	}
-})
+}
