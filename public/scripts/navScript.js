b1c1d4a94d4a3aed92f903f32ef2f6b957d8de9e
Justin <whackybeanz@gmail.com>
2019-08-23 18:26:50 +0800

    Updated dark mode
    - Pages now darken as well instead of turning light grey in dark mode

--- public/scripts/navScript.js
+++ public/scripts/navScript.js
@@ -2,11 +2,15 @@ $(".toggle-mode").on("click", function() {
 	var modeType = $(this).data("mode");
 
 	if(modeType === "dark-mode") {
-		$("body, .table-bordered, .sidebar, .single-info-page").addClass("dark-mode");
+		$("body, .form-control, .table-bordered, .sidebar, .single-info-page, .radio-select").addClass("dark-mode");
+		$(".navbar").addClass("navbar-dark bg-dark");
+		$(".table-bordered").addClass("table-dark");
 		$(".toggle-dark").removeClass("d-flex").addClass("d-none");
 		$(".toggle-light").removeClass("d-none").addClass("d-flex");
 	} else {
-		$("body, .table-bordered, .sidebar, .single-info-page").removeClass("dark-mode");
+		$("body, .form-control, .table-bordered, .sidebar, .single-info-page").removeClass("dark-mode");
+		$(".navbar").removeClass("navbar-dark bg-dark");
+		$(".table-bordered").removeClass("table-dark");
 		$(".toggle-light").removeClass("d-flex").addClass("d-none");
 		$(".toggle-dark").removeClass("d-none").addClass("d-flex");
 	}
