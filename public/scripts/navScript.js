cb4eab4a9370485bf303231308c3ce3956d5068d
Justin Eng <whackybeanz@gmail.com>
2019-08-25 15:25:38 +0800

    Standardized light and dark mode designs

--- public/scripts/navScript.js
+++ public/scripts/navScript.js
@@ -3,13 +3,13 @@ $(".toggle-mode").on("click", function() {
 
 	if(modeType === "dark-mode") {
 		$("body, .form-control, .table-bordered, .sidebar, .single-info-page, .radio-select").addClass("dark-mode");
-		$(".navbar").addClass("navbar-dark bg-dark");
+		$(".navbar").removeClass("navbar-light bg-light").addClass("navbar-dark bg-dark");
 		$(".table-bordered").addClass("table-dark");
 		$(".toggle-dark").removeClass("d-flex").addClass("d-none");
 		$(".toggle-light").removeClass("d-none").addClass("d-flex");
 	} else {
-		$("body, .form-control, .table-bordered, .sidebar, .single-info-page").removeClass("dark-mode");
-		$(".navbar").removeClass("navbar-dark bg-dark");
+		$("body, .form-control, .table-bordered, .sidebar, .single-info-page, .radio-select").removeClass("dark-mode");
+		$(".navbar").removeClass("navbar-dark bg-dark").addClass("navbar-light bg-light");
 		$(".table-bordered").removeClass("table-dark");
 		$(".toggle-light").removeClass("d-flex").addClass("d-none");
 		$(".toggle-dark").removeClass("d-none").addClass("d-flex");
