a770b6f0b23a48fdb382fb134cdc00128807a558
Justin <whackybeanz@gmail.com>
2019-08-30 18:04:59 +0800

    Refactored set item selection window
    - Added sample set item dataset in route
    - Replaced set item selection window to populate based on provided dataset from route
    - Added script to update carousel nav text

--- public/scripts/setItemScript.js
+++ public/scripts/setItemScript.js
@@ -1,8 +1,34 @@
-$(".equip-choice").on("click", function() {
-	$(".hat-choice").removeClass("active");
-	$(this).addClass("active");
+var allItemTypes = ["Hat", "Face Accessory", "Eye Accessory"];
+var currIndex = 0;
 
+$(".equip-choice").on("click", function() {
+	var equipType = $(this).data("equipType");
 	var choiceImage = $(this).data("choiceImg");
 
-	$("#hat-slot").css("background-image", `url("/images/equips/${choiceImage}.png")`);
+	$(`.${equipType}-choice`).removeClass("active");
+	$(this).addClass("active");
+	$(`#${equipType}-slot`).css("background-image", `url(${choiceImage})`);
 })
+
+$(".carousel-control-prev").on("click", function() {
+	currIndex--;
+	updateCarouselNavText(currIndex);
+})
+
+$(".carousel-control-next").on("click", function() {
+	currIndex++;
+	updateCarouselNavText(currIndex);
+})
+
+function updateCarouselNavText(currIndex) {
+	var nextIndex = (currIndex + 1) % allItemTypes.length;
+
+	if(currIndex - 1 < 0) {
+		var prevIndex = allItemTypes.length - 1;
+	} else {
+		var prevIndex = currIndex - 1;
+	}
+
+	$(".carousel-control-prev-text").text(allItemTypes[prevIndex]);
+	$(".carousel-control-next-text").text(allItemTypes[nextIndex]);
+}
--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,56 @@ router.get("/bonus-stats", function(req, res) {
 })
 
 router.get("/set-items", function(req, res) {
-	res.render("setItemIndex");
+	var equipTypes = ["hat", "face", "eye"];
+	var jobTypes = ["warrior", "mage", "archer", "thief", "pirate"];
+
+	var equipsPerJobType = {
+		hat: {
+			warrior: [
+				{image: "/images/equips/acs-warrior-hat.png", name: "Arcaneshade Warrior Hat"},
+				{image: "/images/equips/abs-mage-hat.png", name: "Absolabs Warrior Hat"},
+			],
+			mage: [
+				{image: "/images/equips/acs-warrior-hat.png", name: "Arcaneshade Mage Hat"},
+				{image: "/images/equips/abs-mage-hat.png", name: "Absolabs Mage Hat"},
+			],
+			archer: [
+				{image: "/images/equips/acs-warrior-hat.png", name: "Arcaneshade Mage Hat"},
+			],
+			thief: [
+				{image: "/images/equips/acs-warrior-hat.png", name: "Arcaneshade Mage Hat"},
+			],
+			pirate: [
+				{image: "/images/equips/acs-warrior-hat.png", name: "Arcaneshade Mage Hat"},
+			]
+		},
+		face: {
+			warrior: [
+				{image: "/images/equips/acs-warrior-hat.png", name: "Arcaneshade Warrior Hat"},
+			],
+			mage: [
+				{image: "/images/equips/acs-warrior-hat.png", name: "Arcaneshade Mage Hat"},
+			],
+			archer: [
+				{image: "/images/equips/acs-warrior-hat.png", name: "Arcaneshade Mage Hat"},
+			],
+			thief: [			],
+			pirate: [			]
+		},
+		eye: {
+			warrior: [
+				{image: "/images/equips/acs-warrior-hat.png", name: "Arcaneshade Warrior Hat"},
+			],
+			mage: [
+				{image: "/images/equips/acs-warrior-hat.png", name: "Arcaneshade Mage Hat"},
+			],
+			archer: [			],
+			thief: [			],
+			pirate: [			]
+		}
+	}
+
+	res.render("setItemIndex", {equipTypes: equipTypes, jobTypes: jobTypes, equipsPerJobType: equipsPerJobType});
 })
 
 module.exports = router;
