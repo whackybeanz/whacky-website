8ea56ad6c1e73dc4fd9bd42ea1db4a9deb3fdf26
Justin <whackybeanz@gmail.com>
2019-09-06 16:42:19 +0800

    Added display of possible set effects in breakdown section

--- public/scripts/navScript.js
+++ public/scripts/navScript.js
@@ -3,14 +3,14 @@ $(".toggle-mode").on("click", function() {
 
 	if(modeType === "dark-mode") {
 		$(`body, .sidebar, .single-info-page, .modal, 
-			.table-bordered, .form-control, .radio-select, .job-choice-div`).addClass("dark-mode");
+			.table-bordered, .form-control, .radio-select, .job-choice-div, .set-item-effect-div`).addClass("dark-mode");
 		$(".navbar").removeClass("navbar-light bg-light").addClass("navbar-dark bg-dark");
 		$(".table-bordered").addClass("table-dark");
 		$(".toggle-dark").removeClass("d-flex").addClass("d-none");
 		$(".toggle-light").removeClass("d-none").addClass("d-flex");
 	} else {
 		$(`body, .sidebar, .single-info-page, .modal,
-			.table-bordered, .form-control, .radio-select, .job-choice-div`).removeClass("dark-mode");
+			.table-bordered, .form-control, .radio-select, .job-choice-div, .set-item-effect-div`).removeClass("dark-mode");
 		$(".navbar").removeClass("navbar-dark bg-dark").addClass("navbar-light bg-light");
 		$(".table-bordered").removeClass("table-dark");
 		$(".toggle-light").removeClass("d-flex").addClass("d-none");
--- routes/allSetEffects.js
+++ routes/allSetEffects.js
@@ -5,26 +5,47 @@ var allSetEffects = {
 			hat: {
 				warrior: [{ id: "acs-warrior-hat", image: "/images/equips/acs-knight-hat.png", name: "Arcaneshade Knight Hat" }],
 				mage: [{ id: "acs-mage-hat", image: "/images/equips/acs-mage-hat.png", name: "Arcaneshade Mage Hat" }],
-				archer: [{ id: "acs-archer-hat", image: "/images/equips/acs-archer-hat.png", name: "Arcaneshade Archer Hat" }],
+				archer: [{ id: "acs-archer-hat", image: "/images/equips/acs-archer-hat.png", name: "Arcaneshade Archer Hat" },
+						{ id: "acs-archer-suit", image: "/images/equips/acs-archer-hat.png", name: "Arcaneshade Archer Suit" },
+						{ id: "acs-archer-shoes", image: "/images/equips/acs-archer-hat.png", name: "Arcaneshade Archer Shoes" },
+						{ id: "acs-archer-mitts", image: "/images/equips/acs-archer-hat.png", name: "Arcaneshade Archer Mitts" },
+						{ id: "acs-archer-cape", image: "/images/equips/acs-archer-hat.png", name: "Arcaneshade Archer Cape" },
+						{ id: "acs-archer-shoulder", image: "/images/equips/acs-archer-hat.png", name: "Arcaneshade Archer Shoulder" },
+						{ id: "acs-archer-weapon", image: "/images/equips/acs-archer-hat.png", name: "Arcaneshade Archer Weapon" },],
 				thief: [{ id: "acs-thief-hat", image: "/images/equips/acs-bandit-hat.png", name: "Arcaneshade Bandit Hat" }],
 				pirate: [{ id: "acs-pirate-hat", image: "/images/equips/acs-pirate-hat.png", name: "Arcaneshade Pirate Hat" }],
 			}
 		},
 		effects: { 
 			warrior: {
-				1: "STR +40" 
+				1: [{ statId: "str", statName: "STR", val: "40" }],
+				2: [{ statId: "str", statName: "STR", val: "80" }],
 			},
 			mage: {
-				1: "INT +40"
+				1: [{ statId: "int", statName: "INT", val: "40" }],
+				2: [{ statId: "int", statName: "INT", val: "80" }]
 			},
 			archer: {
-				1: "DEX +40"
+				1: [{ statId: "wa", statName: "ATT", val: "30" }, { statId: "ma", statName: "MATT", val: "30" }, 
+					{ statId: "maxHpMp", statName: "Max HP / Max MP", val: "2000" }],
+				2: [{ statId: "wa", statName: "ATT", val: "35" }, { statId: "ma", statName: "MATT", val: "35" },
+					{ statId: "allStats", statName: "All Stats", val: "50" }],
+				3: [{ statId: "wa", statName: "ATT", val: "40" }, { statId: "ma", statName: "MATT", val: "40" },
+					{ statId: "ied", statName: "Ignore DEF %", val: "10%" }, { statId: "def", statName: "DEF", val: "400" }],
+				4: [{ statId: "wa", statName: "ATT", val: "30" }, { statId: "ma", statName: "MATT", val: "30" },
+					{ statId: "boss", statName: "Boss Damage %", val: "30%" }],
+				5: [{ statId: "wa", statName: "ATT", val: "30" }, { statId: "ma", statName: "MATT", val: "30" },
+					{ statId: "boss", statName: "Max HP / Max MP", val: "30%" }],
+				6: [{ statId: "wa", statName: "ATT", val: "30" }, { statId: "ma", statName: "MATT", val: "30" },
+					{ statId: "ied", statName: "Ignore DEF %", val: "10%" }],
 			},
 			thief: {
-				1: "LUK +40"
+				1: [{ statId: "luk", statName: "LUK", val: 40 }],
+				2: [{ statId: "luk", statName: "LUK", val: 80 }]
 			},
 			pirate: {
-				1: "STR +20\nDEX +20"
+				1: [{ statId: "str", statName: "STR", val: 20 }, { statId: "dex", statName: "DEX", val: 20 }],
+				2: [{ statId: "str", statName: "STR", val: 40 }, { statId: "dex", statName: "DEX", val: 40 }]
 			}
 		}
 	},
@@ -41,19 +62,26 @@ var allSetEffects = {
 		},
 		effects: { 
 			warrior: {
-				1: "STR +40" 
+				1: [{ statId: "str", statName: "STR", val: 40 }],
+				2: [{ statId: "str", statName: "STR", val: 80 }],
 			},
 			mage: {
-				1: "INT +40"
+				1: [{ statId: "int", statName: "INT", val: 40 }],
+				2: [{ statId: "int", statName: "INT", val: 80 }]
 			},
 			archer: {
-				1: "DEX +40"
+				1: [{ statId: "dex", statName: "DEX", val: 40 }],
+				2: [{ statId: "dex", statName: "DEX", val: 80 }]
 			},
 			thief: {
-				1: "LUK +40"
+				1: [{ statId: "luk", statName: "LUK", val: 40 }],
+				2: [{ statId: "luk", statName: "LUK", val: 80 }]
 			},
 			pirate: {
-				1: "STR +20\nDEX +20"
+				1: [{ statId: "str", statName: "STR", val: 20 },
+					{ statId: "dex", statName: "DEX", val: 20 }],
+				2: [{ statId: "str", statName: "STR", val: 40 },
+					{ statId: "dex", statName: "DEX", val: 40 }]
 			}
 		}
 	},
@@ -69,8 +97,8 @@ var allSetEffects = {
 		},
 		effects: { 
 			common: {
-				1: "STR +40",
-				2: "INT +40" 
+				1: [{ statId: "str", statName: "STR", val: 40 }],
+				2: [{ statId: "int", statName: "INT", val: 40 }]
 			}
 		}
 	},
@@ -88,8 +116,8 @@ var allSetEffects = {
 		},
 		effects: { 
 			common: {
-				1: "STR +40", 
-				2: "INT +40"
+				1: [{ statId: "str", statName: "STR", val: 40 }],
+				2: [{ statId: "int", statName: "INT", val: 40 }]
 			}
 		}
 	}
