d31e682d10bb6f5bee0c126a930ccdc8ffdd1c05
Justin <whackybeanz@gmail.com>
2020-03-17 13:24:26 +0800

    Added initial admin route, database function

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 var express 				= require("express");
 var app						= express();
 var bodyParser 				= require("body-parser");
+var mongoose				= require("mongoose");
 
 var indexRoutes 	= require("./routes/index");
+var adminRoutes 	= require("./routes/admin");
 
 var port = process.env.PORT || 3005;
+var databaseUrl = process.env.DATABASEURL || "mongodb://localhost/maple-info";
+mongoose.connect(databaseUrl);
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
 // ROUTES
 app.use("/", indexRoutes);
+app.use("/admin", adminRoutes);
 
 app.get("*", function(req, res) {
 	//req.flash("error", "Invalid route accessed.")
