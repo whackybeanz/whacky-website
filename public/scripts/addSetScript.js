41d61108b571b1ebfc6faf66e1a83e8f5a3ed17c
Justin <whackybeanz@gmail.com>
2020-03-31 18:17:10 +0800

    Created initial handler for adding new set

--- public/scripts/addSetScript.js
+++ public/scripts/addSetScript.js
@@ -5,9 +5,9 @@ $(".add-count-btn").on("click", function() {
 
 	var html = `<div class="form-group row set-effect-input-div" data-num-equipped="${numEquippedCount}">
 					<label for="set-effects" class="col-sm-3 col-form-label">${numEquippedCount} pieces equipped</label>
-					<div class="set-effect-container col-sm-9 px-0 d-flex align-items-start flex-wrap">
-						<div class="w-50 d-flex px-3 mb-2">
-							<div class="add-set-effect-btn btn btn-small btn-outline-secondary col-10 d-flex justify-content-center align-items-center py-1 px-0">
+					<div class="set-effect-container col-sm-9 px-0 d-flex align-items-center flex-wrap" data-num-equipped=${numEquippedCount}>
+						<div class="w-50 d-flex px-3">
+							<div class="add-set-effect-btn btn btn-small btn-outline-secondary col-10 d-flex justify-content-center align-items-center cursor-pointer py-1 px-0" data-num-equipped=${numEquippedCount}>
 								<i class="fas fa-plus"></i>
 							</div>
 						</div>
@@ -15,33 +15,36 @@ $(".add-count-btn").on("click", function() {
 				</div>`;
 
 	$(".num-equipped-container").append(html);
+	$("#num-set-effects").val(numEquippedCount);
 })
 
 $(".num-equipped-container").on("click", ".add-set-effect-btn", function() {
-	var html = `<div class="w-50 d-flex px-3 mb-2">
-					<select class="font-small form-control select-dropdown col-6">
-						<option data-stat-type="str">STR</option>
-						<option data-stat-type="dex">DEX</option>
-						<option data-stat-type="int">INT</option>
-						<option data-stat-type="luk">LUK</option>
-						<option data-stat-type="allStats">All Stats</option>
-						<option data-stat-type="maxHp">Max HP</option>
-						<option data-stat-type="maxMp">Max MP</option>
-						<option data-stat-type="maxHpMp">Max HP/MP</option>
-						<option data-stat-type="maxHpMpPercent">Max HP/MP %</option>
-						<option data-stat-type="def">DEF</option>
-						<option data-stat-type="acc">Accuracy</option>
-						<option data-stat-type="avoid">Avoidability</option>
-						<option data-stat-type="wa">WA</option>
-						<option data-stat-type="ma">MA</option>
-						<option data-stat-type="wama">WA/MA</option>
-						<option data-stat-type="damagePercent">Damage %</option>
-						<option data-stat-type="bossPercent">Boss %</option>
-						<option data-stat-type="iedPercent">IED %</option>
-						<option data-stat-type="critDmgPercent">Crit Damage %</option>
+	var numEquippedCount = $(this).data("numEquipped");
+
+	var html = `<div class="single-effect w-50 d-flex px-3 mb-2">
+					<select class="font-small form-control select-dropdown col-6" name="${numEquippedCount}SetEffectType">
+						<option value="str">STR</option>
+						<option value="dex">DEX</option>
+						<option value="int">INT</option>
+						<option value="luk">LUK</option>
+						<option value="allStats">All Stats</option>
+						<option value="maxHp">Max HP</option>
+						<option value="maxMp">Max MP</option>
+						<option value="maxHpMp">Max HP/MP</option>
+						<option value="maxHpMpPercent">Max HP/MP %</option>
+						<option value="def">DEF</option>
+						<option value="acc">Accuracy</option>
+						<option value="avoid">Avoidability</option>
+						<option value="wa">WA</option>
+						<option value="ma">MA</option>
+						<option value="wama">WA/MA</option>
+						<option value="damagePercent">Damage %</option>
+						<option value="bossPercent">Boss %</option>
+						<option value="iedPercent">IED %</option>
+						<option value="critDmgPercent">Crit Damage %</option>
 					</select>
 					<div class="col-4">
-						<input type="number" class="font-small form-control" placeholder="value">
+						<input type="number" class="font-small form-control" placeholder="value" name="${numEquippedCount}SetEffectValue">
 					</div>
 				</div>`;
 
--- routes/admin.js
+++ routes/admin.js
@@ -10,6 +10,16 @@ router.get("/addSet", function(req, res) {
 	res.render("admin/addSet", {pageView: "add-set"});
 })
 
+router.post("/addSet", function(req, res) {
+	var setData = req.body;
+	console.log(setData)
+
+	Object.values(setData).forEach(function(value) {
+		//console.log(value.length);
+		//console.log(typeof value)
+	})
+})
+
 router.get("/addItem", function(req, res) {
 	res.render("admin/addItem", {pageView: "add-item"});
 })
