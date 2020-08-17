77f65183c5230f950caa60c21f5f8866e4fa601c
Justin Eng <whackybeanz@gmail.com>
2020-08-17 23:42:25 +0800

    Added display of damage skin values in tables
    - Reordered seeds for easier processing

--- models/damageSkinData.js
+++ models/damageSkinData.js
@@ -4,6 +4,15 @@ var damageSkinSchema = new mongoose.Schema({
 	itemId: String,
 	damageSkinId: Number,
 	name: String,
+	hasCounter: { type: Boolean, default: false },
+	hasShot: { type: Boolean, default: false },
+	hasEffect: { type: Boolean, default: true },
+	hasCri0Nums: { type: Boolean, default: true },
+	hasCri1Nums: { type: Boolean, default: true },
+	hasRed1Nums: { type: Boolean, default: true },
+	isCustomSkin: { type: Boolean, default: false },
+	isUnitSkin: { type: Boolean, defualt: false},
+	altNames: [String],
 });
 
 module.exports = mongoose.model("Damage Skin", damageSkinSchema);
