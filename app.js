fc70bc067aa34a86f10909b2d56b9a1b4a097766
Justin Eng <whackybeanz@gmail.com>
2021-01-31 14:58:12 +0800

    Updated site to use absolute paths

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ mongoose.connect(databaseUrl, {useNewUrlParser: true, useUnifiedTopology: true, 
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
-app.use('/public', express.static(__dirname + '/public'));
+app.use(express.static('public'));
 
 // MIDDLEWARE
 app.use(function(req, res, next) {
