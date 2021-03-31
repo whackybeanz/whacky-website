98999c4ab2798de6c7cda2cd7bbff38bd049f35e
Justin Eng <whackybeanz@gmail.com>
2021-03-31 17:59:19 +0800

    Added initial coin shop admin page

--- routes/admin/adminCoinShops.js
+++ routes/admin/adminCoinShops.js
@@ -1,4 +1,11 @@
 var express = require("express");
 var router  = express.Router();
+var middleware = require("../middleware");
+
+router.get("/coin-shops", middleware.isAdmin, function(req, res) {
+    res.locals.extraStylesheet = "adminStyles";
+    res.locals.branch = "coin-shops";
+    res.render("admin/coin-shops/coinShops");
+})
 
 module.exports = router;
