f686a94b12d4d610cbd9852dcf80a87b1696a384
Justin Eng <whackybeanz@gmail.com>
2021-03-31 17:32:57 +0800

    Refactored admin routes
    - Added initial coin shop route file

---
+++ routes/admin/adminIcons.js
@@ -0,0 +1,108 @@
+var AdminHelper = require("../helpers/adminHelpers");
+
+var express = require("express");
+var router  = express.Router();
+var middleware = require("../middleware");
+
+var Icon    = require("../../models/iconData");
+
+router.get("/icons", middleware.isAdmin, function(req, res) {
+    const iconCategories = AdminHelper.getIconCategories();
+    const pageSections = AdminHelper.getPageSections();
+    res.locals.extraStylesheet = "adminStyles";
+    res.locals.branch = "icons";
+    res.render("admin/icons/icons", {iconCategories: iconCategories, pageSections: pageSections});
+})
+
+router.post("/icons", middleware.isAdmin, function(req, res) {
+    const iconData = AdminHelper.compileIconData(req.body);
+
+    Icon.create(iconData, function(err, newIcon) {
+        if(err) {
+            console.log(err);
+            res.redirect("back");
+        } else {
+            console.log(`Created new icon: ${newIcon.name}`);
+            res.redirect("/admin/icons");
+        }
+    })
+})
+
+// Retrieves group of icons based on sorted category.
+// Valid req.params.category values can be found in adminHelpers, getIconCategories() function
+router.get("/icons/:category", middleware.isAdmin, function(req, res) {
+    const selectedCategory = req.params.category;
+
+    Icon.find({ itemType: selectedCategory }, function(err, foundIcons) {
+        if(err) {
+            console.log(err);
+            res.redirect("back");
+        } else {
+            const iconCategories = AdminHelper.getIconCategories();
+            const pageSections = AdminHelper.getPageSections();
+
+            res.locals.extraStylesheet = "adminStyles";
+            res.locals.branch = "icons";
+            res.render("admin/icons/icons", {selectedCategory: selectedCategory, iconCategories: iconCategories, pageSections: pageSections, icons: foundIcons})
+        }
+    })
+})
+
+// Retrieves specific icon based on database ID
+router.get("/icon/:id", middleware.isAdmin, function(req, res) {
+    Icon.findOne({ _id: req.params.id }, function(err, icon) {
+        if(err) {
+            console.log(err);
+            res.redirect("back");
+        } else {
+            const prevUrl = `/admin/icons/${icon.itemType}`;
+            const iconCategories = AdminHelper.getIconCategories();
+            const pageSections = AdminHelper.getPageSections();
+
+            const imgFolder = "/images";
+            const urlIndex = icon.imgUrl.indexOf(imgFolder);
+            const mainPath = icon.imgUrl.slice(0, urlIndex + imgFolder.length);
+            const [mainFolder, subFolder, fileName] = icon.imgUrl.slice(mainPath.length+1).split("/");
+
+            let iconImgUrlSplit = {
+                path: mainPath,
+                mainFolder: mainFolder,
+                subFolder: subFolder,
+                fileName: fileName
+            };
+
+            res.locals.extraStylesheet = "adminStyles";
+            res.locals.branch = "icons";
+            res.render("admin/icons/iconData", {iconCategories: iconCategories, pageSections: pageSections, prevUrl: prevUrl, iconData: icon, iconImgUrl: iconImgUrlSplit });
+        }
+    })
+})
+
+router.post("/icon/:id", middleware.isAdmin, function(req, res) {
+    const newIconData = AdminHelper.compileIconData(req.body); 
+
+    Icon.findOneAndUpdate({ _id: req.params.id }, newIconData, { new: true, overwrite: true }, function(err, updatedIcon) {
+        if(err) {
+            console.log(err);
+            res.redirect("back");
+        } else {
+            const path = updatedIcon.itemType;
+            console.log("Icon updated");
+            res.redirect(`/admin/icons/${path}`);
+        }
+    })
+})
+
+router.post("/icon/:id/delete", middleware.isAdmin, function(req, res) {
+    Icon.findOneAndDelete({ _id: req.params.id }, function(err) {
+        if(err) {
+            console.log(err);
+            res.redirect("back");
+        } else {
+            console.log("Icon deleted");
+            res.redirect("back");
+        }
+    })
+})
+
+module.exports = router;
