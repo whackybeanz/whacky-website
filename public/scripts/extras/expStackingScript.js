1192a6a74b4f3026ac3a1bb37e133380af2cd02c
Justin Eng <whackybeanz@gmail.com>
2021-10-11 00:18:54 +0800

    Added pill navs to EXP stacking page
    - Added EXP table display
    - Disabled unused functions

--- public/scripts/extras/expStackingScript.js
+++ public/scripts/extras/expStackingScript.js
@@ -26,13 +26,66 @@ const patchDetails = {
 }
 
 document.addEventListener("DOMContentLoaded", function(event) {
-    const initSelectedField = document.getElementById("exp-table-used").value;
+    /*const initSelectedField = document.getElementById("exp-table-used").value;
     populateStep1Details(initSelectedField);
     addEXPTableSelectListener();
     addViewTypeBtnListeners();
     addStartCalcBtnListener();
-    addViewEXPTypeListener();
+    addViewEXPTypeListener();*/
+
+    loadEXPTableDetails();
 })
+
+function loadEXPTableDetails() {
+    loadEXPTableHistory();
+    loadCurrentEXPTable();
+}
+
+function loadEXPTableHistory() {
+    EXP_TABLE_HISTORY.forEach(history => {
+        document
+                .getElementById(`exp-table-history`)
+                .insertAdjacentHTML('beforeend', `<tr>
+                    <th scope="row" class="align-middle text-center">${history.name}</th>
+                    <td class="py-2 px-2">${history.details.join("<br/>")}</td>
+                    <td class="align-middle text-center"><a class="text-info" href="${history.url}" target="_blank">Click me!</a></td>
+                </tr>`);
+    })
+}
+
+function loadCurrentEXPTable() {
+    let prevEXP = -1;
+
+    CURR_EXP_TABLE.forEach((levelRange, index) => {
+        let html = `<div class="w-100 col-12 col-sm-6 col-xl-4 d-flex flex-column align-items-center mb-4"><h2 class="font-subsubheader font-weight-bold text-underline mb-2">${levelRange.name}</h2>
+
+        <table class='font-table size-350 table table-sm table-bordered table-hover'>
+            <thead>
+                <tr>
+                    <th scope="col" class="text-center">Level</th>
+                    <th scope="col" class="text-center">EXP To Next Level</th>
+                    <th scope="col" class="text-center">% increase</th>
+                </tr>
+            </thead>
+            <tbody id='exp-table-${index}'>
+            </tbody>
+        </table></div>`;
+
+        document.getElementById("curr-exp-tables").insertAdjacentHTML('beforeend', html);
+
+        levelRange.values.forEach((rawEXP, arrayNum) => {
+            document
+                .getElementById(`exp-table-${index}`)
+                .insertAdjacentHTML('beforeend', `<tr>
+                    <th scope="row" class="text-center">${levelRange.startLevel+arrayNum} > ${levelRange.startLevel+arrayNum+1}</th>
+                    <td class="text-center" id="${levelRange.startLevel+arrayNum}-exp-tnl">${rawEXP.toLocaleString('en-SG')}</td>
+                    <td class="text-center">${prevEXP === -1 ? "-" : (rawEXP / prevEXP * 100 - 100).toFixed(1).replace(/[.,]0$/, "") + "%" }</td>
+                </tr>`);
+
+            prevEXP = rawEXP;
+        })
+    })
+}
 
 // Populates "tooltip" below each input field for patch details and restrictions
 function populateStep1Details(selectedField) {
