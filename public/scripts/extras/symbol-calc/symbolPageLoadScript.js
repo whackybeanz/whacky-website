ff90230ea24032f20f07024ce0b18464f54af5ba
Justin Eng <whackybeanz@gmail.com>
2022-02-02 00:52:44 +0800

    Added symbol cost tables

--- public/scripts/extras/symbol-calc/symbolPageLoadScript.js
+++ public/scripts/extras/symbol-calc/symbolPageLoadScript.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     loadSymbolEXPTables();
+    loadSymbolCostTables();
+    symbolCostTableSelectListener();
 });
 
 function loadSymbolEXPTables() {
@@ -34,5 +36,98 @@ function loadSymbolEXPTables() {
                     <td class="text-center">${totalSymbolEXP.toLocaleString('en-SG')}</td>
                 </tr>`);
         })
+    })
+}
+
+// Create containers for each symbol's cost table data and show RTE's table by default on load
+function loadSymbolCostTables() {
+    SYMBOL_COST_TABLE.forEach(symbol => {
+        document
+            .getElementById("symbol-cost-table-details")
+            .insertAdjacentHTML('beforeend', `<div class="single-symbol-cost-table-div w-100 ${symbol.id === "rte" ? "d-flex" : "d-none"} flex-wrap justify-content-center" id="symbol-${symbol.id}-div"></div>`);
+
+        addCostTable(symbol);
+    })
+}
+
+// Create skeleton table containing headers for each symbol
+function addCostTable(symbol) {
+    let html = `<div class="w-100 col-12 d-flex flex-column align-items-center mb-4"><h2 class="font-subsubheader font-weight-bold text-underline mb-2">${symbol.name}</h2>
+
+    <table class='font-table size-720 table table-sm table-bordered table-hover' id='symbol-${symbol.id}-cost-table'>
+        <thead>
+            <tr>
+                <th scope="col" class="text-center">Level</th>
+                <th scope="col" class="text-center">Cost</th>
+                <th scope="col" class="text-center">Total Cost</th>
+            </tr>
+        </thead>
+        <tbody id='symbol-${symbol.id}-table-details'>
+        </tbody>
+    </table></div>`;
+
+    document.getElementById(`symbol-${symbol.id}-div`).insertAdjacentHTML('beforeend', html);
+
+    populateCostTable(symbol);
+}
+
+/********
+ * Populates each row (level) of symbol upgrade cost.
+ * 
+ * For Arcane Symbols, there are symbol cost adjustments to be noted due to
+ * upcoming DESTINY patch.
+ * 
+ * Display both pre- and post-DESTINY cost details where applicable and percentage change.
+ * ******/
+function populateCostTable() {
+    let totalCost = 0;
+    let totalPostDestinyCost = 0;
+
+    for(let i = 1; i < SYMBOL_MAX_LEVEL[symbol.id]; i++) {
+        let symbolCost = symbol.values[i-1];
+        totalCost += symbolCost;
+
+        let displayedCost = `${symbolCost.toLocaleString('en-SG')}`;;
+        let displayedTotalCost = `${totalCost.toLocaleString('en-SG')}`
+
+        if(symbol.postDestinyValues !== undefined) {
+            let postDestinySymbolCost = symbol.postDestinyValues[i-1]
+            let percentChange = (postDestinySymbolCost - symbolCost) / symbolCost;
+            
+            totalPostDestinyCost += postDestinySymbolCost;
+            let totalPercentChange = (totalPostDestinyCost - totalCost) / totalCost;
+            
+            displayedCost += ` <i class="fas fa-lg fa-long-arrow-alt-right"></i> ${postDestinySymbolCost.toLocaleString('en-SG')}<br/><span class="font-weight-bold text-success">( <i class="fas fa-caret-down"></i> ${(percentChange * 100).toFixed(2)}% )</span>`
+            displayedTotalCost += ` <i class="fas fa-lg fa-long-arrow-alt-right"></i> ${totalPostDestinyCost.toLocaleString('en-SG')}<br/><span class="font-weight-bold text-success">( <i class="fas fa-caret-down"></i> ${(totalPercentChange * 100).toFixed(2)}% )</span>`
+        }
+
+        document
+            .getElementById(`symbol-${symbol.id}-table-details`)
+            .insertAdjacentHTML('beforeend', `<tr>
+                <th scope="row" class="text-center align-middle">${i} > ${i+1}</th>
+                <td scope="row" class="text-center data-potion-raw-cost="" id="symbol-${symbol.id}-level-${i}-cost">
+                    ${displayedCost}
+                </td>
+                <td scope="row" class="text-center data-potion-raw-cost="" id="symbol-${symbol.id}-level-${i}-total-cost">
+                    ${displayedTotalCost}
+                </td>
+            </tr>`);
+    }
+}
+
+// Toggle view of symbol cost chart whenever select option is changed
+function symbolCostTableSelectListener() {
+    const symbolCostTableSelect = document.getElementById("symbol-cost-table-select");
+
+    symbolCostTableSelect.addEventListener("change", function(event) {
+        let currActiveTable = document.querySelector(".single-symbol-cost-table-div.d-flex");
+
+        if(currActiveTable.id !== this.value) {
+            currActiveTable.classList.remove("d-flex");
+            currActiveTable.classList.add("d-none");
+
+            document.getElementById(`symbol-${this.value}-div`).classList.add("d-flex");
+            document.getElementById(`symbol-${this.value}-div`).classList.remove("d-none");
+        }
     })
 }
