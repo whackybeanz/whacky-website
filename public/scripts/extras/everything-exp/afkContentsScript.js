d85bbea46b9f1f8d17ecdb52d90b62ecb1653f40
Justin Eng <whackybeanz@gmail.com>
2022-08-16 20:24:23 +0800

    Added AFK EXP pouch EXP table

--- public/scripts/extras/everything-exp/afkContentsScript.js
+++ public/scripts/extras/everything-exp/afkContentsScript.js
@@ -9,8 +9,16 @@ function loadDojoEXPTable() {
                 .getElementById(`dojo-exp-table`)
                 .insertAdjacentHTML('beforeend', `<tr>
                     <th scope="row" class="text-center align-middle">${startingLevel + index} > ${startingLevel + index + 1}</th>
-                    <td class="text-center align-middle">${expValue.toLocaleString('en-SG')} / tick<br/>${(expValue*12*60).toLocaleString('en-SG')} / hour</td>
+                    <td class="text-center align-middle">${expValue.toLocaleString('en-SG')} EXP / tick<br/>${(expValue*12*60).toLocaleString('en-SG')} EXP / hour</td>
                     <td class="text-center align-middle text-custom font-weight-bold">${(expValue / expTNL*100).toFixed(3) + "%"} / tick<br/>${(expValue*12*60 / expTNL*100).toFixed(3) + "%"} / hour</td>
+                </tr>`);
+
+        document
+                .getElementById(`exp-pouch-table`)
+                .insertAdjacentHTML('beforeend', `<tr>
+                    <th scope="row" class="text-center align-middle">${startingLevel + index} > ${startingLevel + index + 1}</th>
+                    <td class="text-center align-middle">${Math.round(expValue*0.8).toLocaleString('en-SG')} EXP / tick online<br/>${Math.round(expValue*0.8*6*60).toLocaleString('en-SG')} EXP / hour online</td>
+                    <td class="text-center align-middle text-custom font-weight-bold">${(Math.round(expValue*0.8) / expTNL*100).toFixed(3) + "%"} / tick online<br/>${(Math.round(expValue*0.8*6*60) / expTNL*100).toFixed(3) + "%"} / hour online</td>
                 </tr>`);
     })
 
@@ -19,5 +27,12 @@ function loadDojoEXPTable() {
         .insertAdjacentHTML('beforeend', `<tr>
             <th scope="row" class="text-center align-middle">220+</th>
             <td class="text-center align-middle" colspan="2">EXP per tick is same as 219 > 220. It's not worth AFK-ing for EXP beyond this point f3</td>
+        </tr>`);
+
+    document
+        .getElementById(`exp-pouch-table`)
+        .insertAdjacentHTML('beforeend', `<tr>
+            <th scope="row" class="text-center align-middle">220+</th>
+            <td class="text-center align-middle" colspan="2">EXP per tick is same as 219 > 220. Just... collect the pouch whenever you can~</td>
         </tr>`);
 }
